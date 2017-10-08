8308e21913e874d13d34f2fb3bd58a423ed86fa3
Alic Szecsei <aszecsei@gmail.com>
2017-10-08 20:31:40 +0000

    Complete assignment

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -28,7 +28,7 @@ Given /^I am on the RottenPotatoes home page$/ do
    click_on "More about #{title}"
  end
 
- Then /^(?:|I )should see "([^"]*)"$/ do |text|
+ Then /^(?:|I )should see "([^\"]*)"$/ do |text|
     expect(page).to have_content(text)
  end
 
@@ -46,29 +46,56 @@ Given /^I am on the RottenPotatoes home page$/ do
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies have been added to RottenPotatoes:/ do |movies_table|
-  pending  # Remove this statement when you finish implementing the test step
   movies_table.hashes.each do |movie|
-    # Each returned movie will be a hash representing one row of the movies_table
-    # The keys will be the table headers and the values will be the row contents.
-    # Entries can be directly to the database with ActiveRecord methods
-    # Add the necessary Active Record call(s) to populate the database.
+    Movie.create(movie)
   end
+  @movies_length = movies_table.hashes.size
 end
 
 When /^I have opted to see movies rated: "(.*?)"$/ do |arg1|
   # HINT: use String#split to split up the rating_list, then
   # iterate over the ratings and check/uncheck the ratings
   # using the appropriate Capybara command(s)
-  pending  #remove this statement after implementing the test step
+  arg1.split(%r{,\s*}).each do |rating|
+      check("ratings_#{rating}")
+  end
+  click_button('Refresh')
 end
 
 Then /^I should see only movies rated: "(.*?)"$/ do |arg1|
-  pending  #remove this statement after implementing the test step
+    result=true
+    within("tbody") do
+       all("tr").each do |tr|
+            mResult = false
+            arg1.split(%r{,\s*}).each do |rating|
+                if !tr.has_content?(rating)
+                    mResult = true
+                    break
+                end
+            end
+            result = result and mResult
+        end  
+   end
+   expect(result).to be_truthy
 end
 
 Then /^I should see all of the movies$/ do
-  pending  #remove this statement after implementing the test step
+    within("tbody") do
+        all("tr").size.should == @movies_length
+    end
 end
 
+When /^I have sorted movies alphabetically$/ do
+    click_on("Movie Title")
+end
+
+When /^I have sorted movies by date$/ do
+    click_on("Release Date")
+end
+
+Then /^I should see "(.*?)" before "(.*?)"$/ do |movie1, movie2|
+    rows = all("tr")
+    expect(rows.index {|x| x.has_content?(movie1)} < rows.index {|x| x.has_content?(movie2)}).to be_truthy
+end
 
 
